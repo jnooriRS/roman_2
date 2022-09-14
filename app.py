a5ec6ac013337422f88ae2d2c179f765762cb06c
jnooriRS <jonas.noori@riversafe.co.uk>
2022-09-14 10:27:38 +0100

    added templates & working end points

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/home")
-def home():
-    return "home"
+@app.route("/roman/")
+def roman():
+    return render_template("roman.html")
+
+
+@app.route("/arabic/")
+def arabic():
+    return render_template("arabic.html")
 
 
 if __name__ == "__main__":
